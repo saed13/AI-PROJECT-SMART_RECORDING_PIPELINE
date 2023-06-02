544bc37b8530d8ef66fd0b2bfb9def5f31f67d51
Micropot <108392315+Micropot@users.noreply.github.com>
2023-06-02 12:03:18 +0200

    b-plus coding session file

---
+++ 3d_projection/3d_projection.py
@@ -0,0 +1,125 @@
+import json
+import pprint
+from os.path import join
+import glob
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import matplotlib.cm as cm
+import scipy
+
+from helpers import *
+
+with open ('cams_lidars.json', 'r') as f:
+    config = json.load(f)
+
+
+vehicle_view = target_view = config['vehicle']['view']
+cam_fc_view = config['cameras']['front_center']['view']
+lidar_fc_view = config['lidars']['front_center']['view']
+
+cam_fc_axes = get_axes_of_a_view(cam_fc_view)
+lidar_fc_axes = get_axes_of_a_view(lidar_fc_view)
+
+
+cam_fc_to_lidar_fc = transform_from_to(lidar_fc_view, cam_fc_view)
+lidar_fc_to_cam_fc = transform_from_to(cam_fc_view, lidar_fc_view)
+
+##start plotting
+fig, (ax1, ax2, ax3) = plt.subplots(1,3)
+ax1.set_aspect('equal', adjustable='box')
+ax1.set_xlabel('x (front) [m]')
+ax1.set_ylabel('y (side) [m]')
+ax1.set_xlim(0, 60)
+ax1.set_ylim(-18, 18)
+
+
+
+
+
+#ego position
+rect = Rectangle((config['vehicle']['ego-dimensions']['x-range'][0],
+                   config['vehicle']['ego-dimensions']['y-range'][0]),
+                  np.diff(config['vehicle']['ego-dimensions']['x-range']),
+                  np.diff(config['vehicle']['ego-dimensions']['y-range']),
+                  linewidth=1, edgecolor='b', facecolor='none')
+
+#lidar
+file_name_lidar = "camera_lidar/20180810_150607/lidar/cam_front_center/20180810150607_lidar_frontcenter_000000083.npz"
+
+lidar_fc_in_camfc = np.load(file_name_lidar)
+
+
+
+ax1.add_patch(rect)
+lidar_fc_in_global = project_lidar_from_to(lidar_fc_in_camfc, cam_fc_view, vehicle_view)
+
+pc = lidar_fc_in_global['pcloud_points']
+depths = lidar_fc_in_global['pcloud_attr.depth']
+#we say everything lower than half a meter below the lidar is ground.
+pseudo_objects = pc[pc[:,2]>-0.5][:,:2]
+pseudo_ground = pc[pc[:,2]<-0.5][:,:2]
+ax1.scatter(*pseudo_ground.T, s=0.1, c ='g')
+ax1.scatter(*pseudo_objects.T, s=0.1, c ='y')
+#image
+file_name_image = extract_image_file_name_from_lidar_file_name(file_name_lidar)
+img = plt.imread(file_name_image)
+undist_img = undistort_image(img, 'front_center', config)
+undistorted_params = np.array([[0., 0., 0., 0., 0.]])
+ax2.imshow(img)
+pixel_coords = projectPoints(pc, config['cameras']['front_center']['CamMatrix'], undistorted_params)
+
+#with actually distored images use this instead.
+#pixel_coords = projectPoints(pc, config['cameras']['front_center']['CamMatrix'],
+#                                  config['cameras']['front_center']['Distortion'])
+
+
+ax2.scatter(*pixel_coords.T, s=0.2, color=cm.rainbow(1-depths/40))
+
+
+#gather 3d position.
+bounding_box_2d = np.array([[1455, 677],[1915, 930]]) #top left -> bottom right.
+center = np.mean(bounding_box_2d, axis=0)
+idx_nearest_3d_point = np.argmin(np.sum(np.abs(pixel_coords-center),axis=1))
+truck_position = pc[idx_nearest_3d_point]
+truck_marker = plt.Circle((truck_position[0], truck_position[1]), 0.5, color='r')
+ax1.add_patch(truck_marker)
+bounding_box = Rectangle(bounding_box_2d[0],
+        np.diff(bounding_box_2d[:,0]),
+        np.diff(bounding_box_2d[:,1]),
+                  linewidth=1, edgecolor='r', facecolor='none')
+ax2.add_patch(bounding_box)
+
+#road
+border_points_road = [[0,1113], [951,753],[1051,798], [1435, 1205]]
+ctr = np.array(border_points_road).reshape((-1,1,2)).astype(np.int32)
+road_img = cv2.drawContours(img,[ctr],0,(255,255,255),1)
+road_point_mask = [cv2.pointPolygonTest(ctr, point, False) == 1.0 for point in pixel_coords]
+points_within_road_px = pixel_coords[road_point_mask]
+points_within_road_pc = pc[road_point_mask]
+center, normal_vec = planeFit(points_within_road_pc)
+def generate_optimization_fns(normal_vec, center, target_points):
+    v1 = np.cross(normal_vec, (1,0,0))
+    v2 = np.cross(normal_vec, v1)
+    def project_params(x):
+        params = x.reshape(-1,2)
+        points= v1*params[:,0:1] + v2*params[:,1:2] + center
+        points_px = projectPoints(points, config['cameras']['front_center']['CamMatrix'], undistorted_params)
+        return points, points_px
+    def loss_fn(x):
+        points, points_px = project_params(x)
+        return np.linalg.norm(points_px-target_points)
+    return project_params, loss_fn
+project_fn, loss_fn = generate_optimization_fns(normal_vec, center, border_points_road)
+initial_params = np.zeros(len(border_points_road)*2)
+res = scipy.optimize.minimize(loss_fn, initial_params)
+points3d, points_px = project_fn(res.x)
+ax3.scatter(*points_px.T, c='r')
+
+
+plt.scatter(*points_within_road_px.T, s=0.2)
+ax3.imshow(road_img)
+
+
+
+plt.show()
+
---
+++ 3d_projection/helpers.py
@@ -0,0 +1,195 @@
+import numpy as np
+import numpy.linalg as la
+import os.path
+import cv2
+EPSILON = 1e-10
+
+def get_axes_of_a_view(view):
+    x_axis = view['x-axis']
+    y_axis = view['y-axis']
+     
+    x_axis_norm = la.norm(x_axis)
+    y_axis_norm = la.norm(y_axis)
+    
+    if (x_axis_norm < EPSILON or y_axis_norm < EPSILON):
+        raise ValueError("Norm of input vector(s) too small.")
+        
+    # normalize the axes
+    x_axis = x_axis / x_axis_norm
+    y_axis = y_axis / y_axis_norm
+    
+    # make a new y-axis which lies in the original x-y plane, but is orthogonal to x-axis
+    y_axis = y_axis - x_axis * np.dot(y_axis, x_axis)
+ 
+    # create orthogonal z-axis
+    z_axis = np.cross(x_axis, y_axis)
+    
+    # calculate and check y-axis and z-axis norms
+    y_axis_norm = la.norm(y_axis)
+    z_axis_norm = la.norm(z_axis)
+    
+    if (y_axis_norm < EPSILON) or (z_axis_norm < EPSILON):
+        raise ValueError("Norm of view axis vector(s) too small.")
+        
+    # make x/y/z-axes orthonormal
+    y_axis = y_axis / y_axis_norm
+    z_axis = z_axis / z_axis_norm
+    
+    return x_axis, y_axis, z_axis
+
+def get_origin_of_a_view(view):
+    return view['origin']
+
+def get_transform_to_global(view):
+    # get axes
+    x_axis, y_axis, z_axis = get_axes_of_a_view(view)
+
+    # get origin
+    origin = get_origin_of_a_view(view)
+    transform_to_global = np.eye(4)
+
+    # rotation
+    transform_to_global[0:3, 0] = x_axis
+    transform_to_global[0:3, 1] = y_axis
+    transform_to_global[0:3, 2] = z_axis
+
+    # origin
+    transform_to_global[0:3, 3] = origin
+
+    return transform_to_global
+
+def get_transform_from_global(view):
+   # get transform to global
+   transform_to_global = get_transform_to_global(view)
+   trans = np.eye(4)
+   rot = np.transpose(transform_to_global[0:3, 0:3])
+   trans[0:3, 0:3] = rot
+   trans[0:3, 3] = np.dot(rot, -transform_to_global[0:3, 3])
+    
+   return trans
+
+
+def transform_from_to(src, target):
+    transform = np.dot(get_transform_from_global(target), \
+                       get_transform_to_global(src))
+    
+    return transform
+
+def project_lidar_from_to(lidar, src_view, target_view):
+    lidar = dict(lidar)
+    trans = transform_from_to(src_view, target_view)
+    points = lidar['pcloud_points']
+    points_hom = np.ones((points.shape[0], 4))
+    points_hom[:, 0:3] = points
+    points_trans = (np.dot(trans, points_hom.T)).T
+    lidar['pcloud_points'] = points_trans[:,0:3]
+
+    return lidar
+
+def extract_image_file_name_from_lidar_file_name(path_lidar):
+    replace_lidar = lambda x: x if x!='lidar' else 'camera'
+    path_lidar = path_lidar.split('/')
+    path_lidar = list(map(replace_lidar, path_lidar))
+    file_name_image = path_lidar[-1].split('.')[0]
+    file_name_image = file_name_image.split('_')
+    file_name_image = file_name_image[0] + '_' + \
+                        'camera_' + \
+                        file_name_image[2] + '_' + \
+                        file_name_image[3] + '.png'
+
+    return os.path.join("/".join(path_lidar[:-1] + [file_name_image]))
+
+def extract_semantic_file_name_from_image_file_name(file_name_image):
+    file_name_semantic_label = file_name_image.split('/')
+    file_name_semantic_label = file_name_semantic_label[-1].split('.')[0]
+    file_name_semantic_label = file_name_semantic_label.split('_')
+    file_name_semantic_label = file_name_semantic_label[0] + '_' + \
+                  'label_' + \
+                  file_name_semantic_label[2] + '_' + \
+                  file_name_semantic_label[3] + '.png'
+
+    return file_name_semantic_label
+
+def undistort_image(image, cam_name, config):
+    if cam_name in ['front_left', 'front_center', \
+                    'front_right', 'side_left', \
+                    'side_right', 'rear_center']:
+        # get parameters from config file
+        intr_mat_undist = \
+                  np.asarray(config['cameras'][cam_name]['CamMatrix'])
+        intr_mat_dist = \
+                  np.asarray(config['cameras'][cam_name]['CamMatrixOriginal'])
+        dist_parms = \
+                  np.asarray(config['cameras'][cam_name]['Distortion'])
+        lens = config['cameras'][cam_name]['Lens']
+
+        if (lens == 'Fisheye'):
+            return cv2.fisheye.undistortImage(image, intr_mat_dist,\
+                                      D=dist_parms, Knew=intr_mat_undist)
+        elif (lens == 'Telecam'):
+            return cv2.undistort(image, intr_mat_dist, \
+                      distCoeffs=dist_parms, newCameraMatrix=intr_mat_undist)
+        else:
+            return image
+    else:
+        return image
+
+def map_lidar_points_onto_image(image_orig, lidar, pixel_size=3, pixel_opacity=1):
+    image = np.copy(image_orig)
+
+    # get rows and cols
+    rows = (lidar['row'] + 0.5).astype(np.int)
+    cols = (lidar['col'] + 0.5).astype(np.int)
+
+    # lowest distance values to be accounted for in colour code
+    MIN_DISTANCE = np.min(lidar['distance'])
+    # largest distance values to be accounted for in colour code
+    MAX_DISTANCE = np.max(lidar['distance'])
+
+    # get distances
+    distances = lidar['distance']
+    # determine point colours from distance
+    colours = (distances - MIN_DISTANCE) / (MAX_DISTANCE - MIN_DISTANCE)
+    colours = np.asarray([np.asarray(hsv_to_rgb(0.75 * c, \
+                        np.sqrt(pixel_opacity), 1.0)) for c in colours])
+    pixel_rowoffs = np.indices([pixel_size, pixel_size])[0] - pixel_size // 2
+    pixel_coloffs = np.indices([pixel_size, pixel_size])[1] - pixel_size // 2
+    canvas_rows = image.shape[0]
+    canvas_cols = image.shape[1]
+    for i in range(len(rows)):
+        pixel_rows = np.clip(rows[i] + pixel_rowoffs, 0, canvas_rows - 1)
+        pixel_cols = np.clip(cols[i] + pixel_coloffs, 0, canvas_cols - 1)
+        image[pixel_rows, pixel_cols, :] = \
+                (1. - pixel_opacity) * \
+                np.multiply(image[pixel_rows, pixel_cols, :], \
+                colours[i]) + pixel_opacity * 255 * colours[i]
+    return image.astype(np.uint8)
+
+## taken from https://stackoverflow.com/questions/12299540/plane-fitting-to-4-or-more-xyz-points
+def planeFit(points):
+    """
+    p, n = planeFit(points)
+
+    Given an array, points, of shape (d,...)
+    representing points in d-dimensional space,
+    fit an d-dimensional plane to the points.
+    Return a point, p, on the plane (the point-cloud centroid),
+    and the normal, n.
+    """
+    import numpy as np
+    points = np.reshape(points, (np.shape(points)[0], -1)).T # Collapse trialing dimensions
+    assert points.shape[0] <= points.shape[1], "There are only {} points in {} dimensions.".format(points.shape[1], points.shape[0])
+    ctr = points.mean(axis=1)
+    x = points - ctr[:,np.newaxis]
+    M = np.dot(x, x.T) # Could also use np.cov(x) here.
+    return ctr, la.svd(M)[0][:,-1]
+
+def projectPoints(points, camMtx, dist):
+    pose_to_cam_coord_transform = np.array([[0, -1, 0], [0, 0, -1], [1, 0, 0]])
+    pixel_coords, _ = cv2.projectPoints(pose_to_cam_coord_transform.dot(points.T).T,
+                      np.asarray([[0,0,0]], dtype=np.float64),
+                      np.asarray([[0,0,0]], dtype=np.float64),
+                      np.asarray(camMtx),
+                      np.asarray(dist))
+    pixel_coords = pixel_coords.squeeze()
+    return pixel_coords
